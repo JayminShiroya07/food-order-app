354866b019564807f4db5682a97fc67ccd73e015
jaymin shiroya <jaymeenshiroya399@gmail.com>
2025-02-14 16:23:39 +0530

    Implement  UI of meal listing feature with header and meal item components

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({children, textOnly, className,...props}){
+
+    const cssClasses = className +" "+ (textOnly ? 'text-button' : 'button');
+
+    return(
+        <button className={cssClasses} {...props}>
+            {children}
+        </button>
+    )
+}
